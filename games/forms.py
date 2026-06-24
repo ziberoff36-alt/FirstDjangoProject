0705c04c70d2437ee51ba1472ca5af5266f6ca4f
Николай <Ziberoff36@gmail.com>
2026-06-24 16:53:28 +0300

    Завершено создание GameForm

--- games/forms.py
+++ games/forms.py
@@ -1,7 +1,38 @@
-from django .forms import forms
-from . import Game
+from django import forms
+from .models import Game
+
 
 class GameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ['name', 'year', 'rating', 'platform']
+
+        labels = {
+            'name': 'Название игры',
+            'year': 'Год выпуска',
+            'rating': 'Рейтинг (от 0 до 10)',
+            'platform': 'Платформа',
+        }
+
+        help_texts = {
+            'year': 'Например: 2024',
+            'rating': 'Можно вводить дробные числа, например: 8.5',
+            'platform': 'Выберите платформу из списка (необязательно)',
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Введите название игры...',
+            }),
+            'year': forms.NumberInput(attrs={
+                'min': 1950,
+                'max': 2026,
+                'placeholder': 'гггг'
+            }),
+            'rating': forms.NumberInput(attrs={
+                'min': 0,
+                'max': 10,
+                'step': 0.1,
+                'placeholder': '0.0'
+            }),
+        }
