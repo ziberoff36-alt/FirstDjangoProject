00f5fda8a85f8a42045918a249e72056e4d22b3c
Николай <Ziberoff36@gmail.com>
2026-06-24 16:52:32 +0300

    Добавлена ссылка на GameForm

--- games/urls.py
+++ games/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.GameListView.as_view(), name='game_list'),
+    path('add/', views.GameCreateView.as_view(), name='game_create'),
     path('<int:pk>/', views.GameDetailView.as_view(), name='game_detail'),
     path('platform/', views.PlatformListView.as_view(), name='platform_list'),
     path('platform/<int:pk>/', views.GamesByPlatformListView.as_view(), name='games_by_platform'),
