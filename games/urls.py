f4ded65cb529a56126cbab67c16dd5f5f8242342
Николай <Ziberoff36@gmail.com>
2026-06-24 17:45:36 +0300

    Добавлена ссылка для GameDeleteView

--- games/urls.py
+++ games/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.GameListView.as_view(), name='game_list'),
     path('add/', views.GameCreateView.as_view(), name='game_create'),
+    path('<int:pk>/delete/', views.GameDeleteView.as_view(), name='game_delete'),
     path('<int:pk>/', views.GameDetailView.as_view(), name='game_detail'),
     path('platform/', views.PlatformListView.as_view(), name='platform_list'),
     path('platform/<int:pk>/', views.GamesByPlatformListView.as_view(), name='games_by_platform'),
